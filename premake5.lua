dc7d312878c3bf44a7d5fc0658ba1b75f4d65bcd
contentseeker10 <appelsene.design@gmail.com>
2026-01-26 14:25:14 +0200

    Migrated build system to Premake; added Dist config
    Switched to Premake for project generation and build management. Added Dist configuration to both Sandbox and Taurida projects. Updated .vcxproj files with new GUIDs, output directories, and C++17 settings. Simplified .filters files and regenerated solution file to match Premake conventions. Added post-build DLL copy for Taurida. Modernized and clarified build setup for multi-config support.

---
+++ premake5.lua
@@ -0,0 +1,109 @@
+workspace "Taurida"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Taurida"
+	location "Taurida"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	buildoptions "/utf-8"
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines 
+		{
+			"TRD_PLATFORM_WINDOWS",
+			"TRD_BUILD_DLL"
+		}
+
+		postbuildcommands 
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "TRD_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "TRD_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Taurida/vendor/spdlog/include",
+		"Taurida/src"
+	}
+
+	links 
+	{
+		"Taurida"
+	}
+
+	buildoptions "/utf-8"
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines 
+		{
+			"TRD_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "TRD_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "TRD_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		optimize "On"
